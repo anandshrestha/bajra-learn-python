e44e47aa4390bffa5ddb945f6aff7e7961c5a998
BCA <anandshrestha18@gmail.com>
2020-10-08 15:07:52 +0545

    if else statement

--- if.py
+++ if.py
@@ -1,1 +1,29 @@
+"""learning simple if else"""
 #learning python
+# age =int(input("Enter your present age"))
+# if age > 18:
+#     print("You are 18+")
+# elif age == 18:
+#     print("You are equal to 18")
+# else:
+#     print("You are below 18")
+"""learning single line if else"""
+# Grade =int(input("Enter Your grade:"))
+# result = "pass" if Grade > 60 else "fail"
+# print(result)
+"""learning nested if else"""
+grade =int(input("Enter your grade"))
+if grade > 40:
+    if grade >= 60:
+        if grade < 80:
+            print ("first division")
+    elif grade >= 80 and grade < 70:
+        print("distintion")
+    else:
+        print("second division")
+else:
+        print("fail")
+        
+
+
+ 
