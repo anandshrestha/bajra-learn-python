a4fc666c7e38249c33b108ca2a1db91988fc698e
BCA <anandshrestha18@gmail.com>
2020-10-08 14:26:23 +0545

    if python comment

---
+++ 1.py
@@ -0,0 +1,9 @@
+print("Enter your name")
+a=input()
+print ("Enter your favorite food")
+b=input()
+print("Enter your qualification")
+c=input()
+print("Enter your favorite fruit")
+d=input()
+
--- test.py
+++ test.py
@@ -1,2 +1,6 @@
 print("Hello World")
 print("Hello Nepal!")
+x = 33
+y = 200
+if y > x:
+  print("y is greater than x")
